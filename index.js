c6447d3ede902248f61dd183cac2b048303b80eb
ikpajustice500-bit <ikpajustice500@gmail.com>
2025-12-27 12:59:45 +0100

    new changes

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const name ="Ikpa justice";
 const age = 20;
 const favoriteFood = "Yam";
 
-console.log('Name: ${name}')
-console.log('Age: ${age}')
-console.log('Favorite Food: ${favoriteFood}')
+console.log(`Name: ${name}`)
+console.log(`Age: ${age}`)
+console.log(`Favorite Food: ${favoriteFood}`)
